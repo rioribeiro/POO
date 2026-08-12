473646f4af3bbcee73af3e101c4ccc05116a5d1a
Rio Ribeiro da Silva <ribeiro.silva1@academico.ifrn.edu.br>
2026-08-12 10:19:45 -0300

    exeum.java

---
+++ lista02/exeum.java
@@ -0,0 +1,29 @@
+package lista02; // Resolve o primeiro erro
+
+import java.util.Scanner; // Resolve o segundo erro
+
+public class exeum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int somaPares = 0;
+        int somaImpares = 0;
+        
+        System.out.println("Digite quatro valores inteiros:");
+        
+        for (int i = 0; i < 4; i++) {
+            int num = input.nextInt();
+            
+            if (num % 2 == 0) {
+                somaPares += num;
+            } else {
+                somaImpares += num;
+            }
+        }
+        
+        System.out.println("Soma dos pares = " + somaPares);
+        System.out.println("Soma dos ímpares = " + somaImpares);
+        
+        input.close();
+    }
+}
