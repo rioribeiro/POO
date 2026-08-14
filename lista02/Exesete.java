d393a80bce38fe2ddaa4e8948a207d869f5c8891
Rio Ribeiro <rioribeirods@gmail.com>
2026-08-14 16:58:27 -0300

    Exesete.java

---
+++ lista02/Exesete.java
@@ -0,0 +1,26 @@
+package lista02;
+
+import java.util.Scanner;
+
+public class Exesete {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite uma frase:");
+        String frase = sc.nextLine();
+
+        // Quebra a frase em palavras
+        String[] palavras = frase.split(" ");
+
+        // Imprime removendo uma palavra do início a cada vez
+        for (int i = 0; i < palavras.length; i++) {
+            for (int j = i; j < palavras.length; j++) {
+                System.out.print(palavras[j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
